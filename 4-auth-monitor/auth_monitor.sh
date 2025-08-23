512fdfa278a8f662687c42a7a91e1a4d47224a53
Saketh <saketh9977@gmail.com>
2025-08-23 11:34:02 +0530

    auth_monitor: went through all logs of following loginwindow categories: loginsupport, Standard, KeyMilestone for case - 2users-2success

--- 4-auth-monitor/auth_monitor.sh
+++ 4-auth-monitor/auth_monitor.sh
@@ -17,13 +17,16 @@ echo $USER
 
 # tested on macOS 15.5 with M1 chip.
 
+# loginwindow: (loginsupport)
+# loginwindow: [com.apple.loginwindow.logging:Standard]
+# loginwindow: [com.apple.loginwindow.logging:KeyMilestone]
+
 
 log stream --info --debug \
   --predicate '(
     (
       process == "loginwindow" AND (
         composedMessage CONTAINS[c] "loginPressed," OR
-        composedMessage CONTAINS[c] "Attempt #" OR
         composedMessage CONTAINS[c] "Auth began" OR
         composedMessage CONTAINS[c] "auth started with lock UI showing" OR
         composedMessage CONTAINS[c] "to prevent further interaction" OR
@@ -31,7 +34,27 @@ log stream --info --debug \
         composedMessage CONTAINS[c] "Screensaver authorization failed" OR
         composedMessage CONTAINS[c] "INCORRECT password" OR
         composedMessage CONTAINS[c] "lock UI is showing" OR
-        composedMessage CONTAINS[c] "focused user name: saketh"
+        composedMessage CONTAINS[c] "focused user name:" OR
+        composedMessage CONTAINS[c] "attempting login for user:" OR
+        composedMessage CONTAINS[c] "is logged in" OR
+        composedMessage CONTAINS[c] "Showing the user avatar for" OR
+        composedMessage CONTAINS[c] "switching to user" OR
+        composedMessage CONTAINS[c] "newUserOnConsole" OR
+        composedMessage CONTAINS[c] "processing lock screen request" OR
+        composedMessage CONTAINS[c] "immediate lock" OR
+        composedMessage CONTAINS[c] "loginwindow sending screen is locked notification" OR
+        composedMessage CONTAINS[c] "sessionUnlocked : 0" OR
+        composedMessage CONTAINS[c] "ess_notify_lw_session_lock" OR
+        composedMessage CONTAINS[c] "processing unlock screen request" OR
+        composedMessage CONTAINS[c] "new state: Desktop showing" OR
+        composedMessage CONTAINS[c] "sendNotificationOf kScreenIsUnlocked" OR
+        composedMessage CONTAINS[c] "sending screen is locked notification" OR
+        composedMessage CONTAINS[c] "new state: Shield window showing" OR
+        composedMessage CONTAINS[c] "login state: LoginwindowUIShown" OR
+        composedMessage CONTAINS[c] "tearing down screen lock space" OR
+        composedMessage CONTAINS[c] "ess_notify_lw_session_unlock" OR
+        composedMessage CONTAINS[c] "enter.  userName:" OR
+        composedMessage CONTAINS[c] "Enter. Prefs: "
       )
     ) OR 
     (
@@ -45,11 +68,8 @@ log stream --info --debug \
     /loginPressed,/ {
       print $1, substr($2, 1, 8), substr($2, index($2, "+")), "login button clicked"
     }
-    /Attempt #/ {
-      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "Attempt", $NF
-    }
-    /focused user name: saketh/ {
-      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "user=saketh"
+    /focused user name: .*/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "focused user name:", $NF
     }
     /Auth began/ {
       print $1, substr($2, 1, 8), substr($2, index($2, "+")), "validating..."
@@ -77,6 +97,63 @@ log stream --info --debug \
     }
     /Device lock state \(locked\): 0/ {
       print $1, substr($2, 1, 8), substr($2, index($2, "+")), "unlocked (homed)"
+    }
+    /attempting login for user: .*/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "attempting login for user:", $NF
+    }
+    /User .* is logged in/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "User", $(NF-3), "is logged in"
+    }
+    /Showing the user avatar for .*/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "Showing the user avatar for", $NF
+    }
+    /switching to user/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "switching to user:", $(NF-1)
+    }
+    /newUserOnConsole: .*/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "newUserOnConsole:", $NF
+    }
+    /processing lock screen request/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "processing lock screen request"
+    }
+    /immediate lock/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "lock immediately"
+    }
+    /loginwindow sending screen is locked notification|sending screen is locked notification/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "screen is locked notification"
+    }
+    /sessionUnlocked : 0/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "session locked"
+    }
+    /ess_notify_lw_session_lock/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "session locked notification"
+    }
+    /processing unlock screen request/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "processing unlock screen request"
+    }
+    /new state: Desktop showing/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "showing desktop"
+    }
+    /sendNotificationOf kScreenIsUnlocked/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "screen is unlocked notification"
+    }
+    /new state: Shield window showing/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "showing shield window"
+    }
+    /login state: LoginwindowUIShown/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "showing login window"
+    }
+    /tearing down screen lock space/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "removing screen lock"
+    }
+    /ess_notify_lw_session_unlock/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "session unlock notification"
+    }
+    /enter\.  userName:/ {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "settingup", $(NF-3)
+    }
+    /Enter\. Prefs: / {
+      print $1, substr($2, 1, 8), substr($2, index($2, "+")), "settingup prefs for", $(NF-1)
     }
 '
 
